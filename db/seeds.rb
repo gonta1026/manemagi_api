eb87c406c2a0e9227b89675515bcab8ab55e6e7b
gonta1026 <katano.original@gmail.com>
2021-06-26 21:13:02 +0900

    feat: seedファイルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_id = 1
+shop_id = 1
+User.create!(
+  email: "text_user@example.com",
+  password: "11111111",
+  password_confirmation: "11111111"
+)
+Shop.create!(
+  name: "万台",
+  description: "生鮮品がいい",
+  user_id: user_id
+)
+
+2.times do |n|
+  Shopping.create!(
+    price: 1000,
+    date: "2021-06-12 01:55:11.633042",
+    description: "aaa",
+    is_line_notice: false,
+    shop_id: shop_id,
+    user_id: user_id
+  )
+end
+
+Claim.create!(
+  is_line_notice: true,
+  user_id: user_id
+)
