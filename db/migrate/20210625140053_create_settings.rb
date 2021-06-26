d41fd0e477125ba9320b0ceb53f1ad635ba33c8f
gonta1026 <katano.original@gmail.com>
2021-06-26 22:32:40 +0900

    update: uniqueオプションを追加

--- db/migrate/20210625140053_create_settings.rb
+++ db/migrate/20210625140053_create_settings.rb
@@ -2,6 +2,7 @@ class CreateSettings < ActiveRecord::Migration[6.1]
   def change
     create_table :settings do |t|
       t.boolean :is_use_line, null: false, default: false
+      t.string :line_notice_token, unique:true
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
--- db/migrate/20210625140630_create_shops.rb
+++ db/migrate/20210625140630_create_shops.rb
@@ -1,7 +1,7 @@
 class CreateShops < ActiveRecord::Migration[6.1]
   def change
     create_table :shops do |t|
-      t.string :name, null: false 
+      t.string :name, null: false, unique:true
       t.text :description
       t.references :user, null: false , foreign_key: true
       t.timestamps
