3c53db049bbfaf49fcdd243a074fb7409b4bd091
gonta1026 <katano.original@gmail.com>
2021-07-02 00:17:56 +0900

    update: headerをクライアントで確認をできるように設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "*"
     resource "*",
       headers: :any,
+      expose: ['access-token', 'uid', "client"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
         # credentials: true
   end
