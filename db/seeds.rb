0b2b63fe48215b672a21ffff0d8aa75791e965ab
gonta1026 <katano.original@gmail.com>
2021-07-10 13:27:39 +0900

    update: 各ユーザーごとの設定ができるようにseedファイルを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,44 @@
-user = User.create!(
-  email: "text_user@example.com",
-  password: "11111111",
-  password_confirmation: "11111111"
-)
-shop = Shop.create!(
-  name: "万台",
-  description: "生鮮品がいい",
-  user_id: user.id
-)
+members = ["otsuka", "nono", "syun", "fukada", "uirou"]
+members.each do |member|
+  user = User.create(
+    name: "#{member}",
+    email: "#{member}@example.com",
+    password: "11111111",
+    password_confirmation: "11111111"
+  )
 
-shoppings = []
-2.times do |n|
-  shopping = Shopping.create!(
-    price: 1000,
-    date: "2021-06-12 01:55:11.633042",
-    description: "説明",
-    is_line_notice: false,
-    shop_id: shop.id,
+  Setting.create(
+    is_use_line: false,
+    line_notice_token: nil,
     user_id: user.id
   )
-  shoppings.push(shopping)
-end
-
-claim = Claim.create!(
-  is_line_notice: true,
-  user_id: user.id
-)
-
-shoppings.each do |shopping|
-  shopping.update!(claim_id: claim.id)
+  
+  shop = Shop.create(
+    name: "万台",
+    description: "生鮮品がいい",
+    user_id: user.id
+  )
+  
+  shoppings = []
+  5.times do |n|
+    shopping = Shopping.create(
+      price: 1000 * (n + 1),
+      date: "2021-06-0#{(n + 1)}",
+      description: "説明",
+      is_line_notice: false,
+      shop_id: shop.id,
+      user_id: user.id
+    )
+    shoppings.push(shopping)
+  end
+  
+  claim = Claim.create(
+    is_line_notice: true,
+    user_id: user.id,
+    shoppings: shoppings
+  )
+  
+  # shoppings.each do |shopping|
+  #   shopping.update!(claim_id: claim.id)
+  # end
 end
