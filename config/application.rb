bff05bde539c3d7e66acd26a88056e77556a7840
gonta1026 <katano.original@gmail.com>
2021-07-10 13:24:49 +0900

    update: time_zoneの設定をしてDBへの保存時間を変更

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Manemagi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
     #
