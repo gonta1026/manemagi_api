3c538fbf5bf3efe86416580dff58af495f72ca38
gonta1026 <katano.original@gmail.com>
2021-07-20 00:42:02 +0900

    update: 設定画面の修正

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -13,17 +13,17 @@ class SettingsController < ApplicationController
         line_notice_token: setting.line_notice_token
       }
     }
-    render json: { status: 'SUCCESS', data: { user: user }}
+    render json: { status: 'SUCCESS', data: user}
   end
 
   def update
-    @setting = Setting.find(params[:id])
+    @setting = Setting.find(current_user.setting.id)
     if @setting.update(post_params)
-      if !@setting.line_notice_token.nil?
+      if !@setting.line_notice_token.blank?
         response = @setting.line_token_check(@setting.line_notice_token)
         if response.status != 200
           # 設定情報のtokenをセットしたらtokenが誤ってエラーになる。
-          render json: { status: 'ERROR', data: response }
+          render json: { status: 'ERROR', data: response, message: "invalid_token" }
         else
           # 設定情報のtokenをセットて正常の処理ができた
           render json: { status: 'SUCCESS', data: @setting }
