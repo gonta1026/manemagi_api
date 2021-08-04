22b8501aaed553a73b8e85d9e11eda63abaeda75
gonta1026 <katano.original@gmail.com>
2021-08-05 06:47:14 +0900

    feat: 請求受領の実装

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,5 +1,5 @@
 class ClaimsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new, :create, :shoppings]
+  before_action :authenticate_user!, only: [:index, :new, :create, :update, :shoppings]
 
   def index
     claims = current_user.claims
@@ -29,15 +29,28 @@ class ClaimsController < ApplicationController
     end
   end
 
+  def update  
+    @claim = current_user.claims.find(params[:id])    
+    if @claim.update(update_params)
+      render json: { status: 'success', data: @claim }
+    else
+      render json: { status: 'error', data: @claim.errors }
+    end
+  end
+
   # 請求に紐づく買い物一覧画面
   def shoppings
     claim = Claim.find(params[:id])
     shoppings = claim.shoppings
     render json: { status: 'success', data: shoppings }
   end
+  
   private
-
   def post_params
     params.require(:claim).permit(:is_line_notice).merge(user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:claim).permit(:is_get_claim)
   end
 end
