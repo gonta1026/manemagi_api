d1b4d25edaf168e28d0a47a9220d760514f55cd2
gonta1026 <katano.original@gmail.com>
2021-08-10 22:47:20 +0900

    fix: カラム名をis_receiptに変更

--- db/migrate/20210625143254_create_claims.rb
+++ db/migrate/20210625143254_create_claims.rb
@@ -2,7 +2,7 @@ class CreateClaims < ActiveRecord::Migration[6.1]
   def change
     create_table :claims do |t|
       t.boolean :is_line_notice, null: false, default: false
-      t.boolean :is_get_claim, null: false, default: false
+      t.boolean :is_receipt, null: false, default: false
       t.references :user, foreign_key: true
       t.timestamps
     end
